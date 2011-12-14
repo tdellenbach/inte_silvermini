f53d5aa08da9caa744f75d871d3aa8d6f0839932
Yannick Wyss <ywyss@hsr.ch>
2011-12-14 17:37:49 +0100

    code examples from other teams add (and some dll's)

---
+++ IntTeTestat/WelcomePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace IntTeTestat
+{
+    public partial class WelcomePage : UserControl
+    {
+
+        public WelcomePage()
+        {
+            InitializeComponent();
+            WarningTextBlock.Visibility = Visibility.Collapsed;
+        }
+
+        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UsernameTextBox.Text == "")
+            {
+                WarningTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+               /// TODO check if enough players, load the waitingPage or the gamePage, how can we invite the other players to the game?
+                Frame contentFrame = (Frame) this.Parent;
+                contentFrame.Content = new WaitingPage();
+            }
+        }
+
+    }
+}
